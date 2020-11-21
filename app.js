15d73377fa18efffe96c1039b3b8cdc9e9361ab3
Rowdy Goat <dbrockman28@gmail.com>
2020-11-20 19:05:12 -0600

    added validation

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "What is your name?"
+      message: "What is your name?",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      }
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub username"
+      message: "Enter your GitHub username",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      }
     },
     {
       type: "input",
@@ -36,12 +52,28 @@ const promptProject = portfolioData => {
     {
       type: "input",
       name: "name",
-      message: "What is the name of your project?"
+      message: "What is the name of your project?",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your project's name!");
+          return false;
+        }
+      }
     },
     {
       type: "input",
       name: "description",
-      message: "Provide a description of the project (Required)"
+      message: "Provide a description of the project (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please describe your project!");
+          return false;
+        }
+      }
     },
     {
       type: "checkbox",
@@ -52,7 +84,15 @@ const promptProject = portfolioData => {
     {
       type: "input",
       name: "link",
-      message: "Enter the GitHub link to your project. (Required)"
+      message: "Enter the GitHub link to your project. (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter a link to your project!");
+          return false;
+        }
+      }
     },
     {
       type: "confirm",
